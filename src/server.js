109a6268f871b7d380e6431d5a26ebc87555da17
hussainjaura <0jaurm68@solent.ac.uk>
2025-05-10 16:47:56 +0100

    server is now running on render.com

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 // used helmet to secure HTTP headers
 app.use(helmet());
 
